7be28194822b32407ce12856ee5fb56e77f0d59a
krishna_ink <krishnakanth4477@gmail.com>
2024-05-18 19:53:00 -0400

    Finish home component

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -3,16 +3,16 @@ import React from 'react'
 const Social = () => {
   return (
     <div className="home__social">
-        <a href="https://www.github.com/" className="home__social-icon" target="_blank">
-            <i class="uil uil-instagram"> </i>
+        <a href="https://www.instagram.com/" className="home__social-icon" target="_blank">
+            <i className="uil uil-instagram"> </i>
         </a>
 
         <a href="https://www.instagram.com/" className="home__social-icon" target="_blank">
-            <i class="uil uil-instagram"> </i>
+            <i className="uil uil-instagram"> </i>
         </a>
 
-        <a href="" className="https://www.instagram.com/" target="_blank">
-            <i class="uil uil-github"> </i>
+        <a href="https://www.github.com/" className="home__social-icon" target="_blank">
+            <i className="uil uil-github"> </i>
         </a>
 
     </div>
